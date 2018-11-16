6ac380b4d44dc990bde71c130ebbb7d3e79be3fc
苏庆林 <suqinglin@qizidai.com>
2018-11-16 11:17:18 +0800

    Initial Commit

---
+++ LittleTmall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LittleTmall
+//
+//  Created by Adam on 2018/11/16.
+//  Copyright © 2018年 com.qzxq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LittleTmall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LittleTmall
+//
+//  Created by Adam on 2018/11/16.
+//  Copyright © 2018年 com.qzxq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
